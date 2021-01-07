96c3505058f902af875a68cfa7b8af6b22aef02b
Daan Carstens <28642@ma-web.nl>
2021-01-07 16:17:22 +0100

    video 1 af

---
+++ iO.js
@@ -0,0 +1,22 @@
+let alleLinks = document.querySelectorAll('nav a');
+
+const verwijderActief = () => {
+    alleLinks.forEach ( (link) => {
+        if( link.classList = 'actief' ) {
+            link.classList.remove('actief');
+        }
+    });
+}
+
+const maakActief = (elem) => {
+    verwijderActief();
+    elem.classList.add('actief');
+}
+
+alleLinks.forEach( (link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        maakActief(e.target);
+        window.location = e.target.href;
+    })
+});
