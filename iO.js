c699146c16d51292097d698ccd906a50bd27f30d
Daan Carstens <28642@ma-web.nl>
2021-01-07 18:41:05 +0100

    project af

--- iO.js
+++ iO.js
@@ -5,7 +5,6 @@ const opties = {};
 
 const verwerkDoorsnijding = (entries, observer) => {
     entries.forEach(entry => {
-        // console.log(entry.target.id + " doorsnijdt " + entry.isIntersecting);
         if ( entry.isIntersecting ) {
            let link = zoekBijpassendeLink('#' + entry.targeet.id);
             maakActief(link);
@@ -15,11 +14,13 @@ const verwerkDoorsnijding = (entries, observer) => {
 
 let observer = new IntersectionObserver(verwerkDoorsnijding, opties);
 
-observer.observe(deSecties[1]);
+deSecties.forEach(sectie => {
+    observer.observe(sectie.getElementsByTagName('p')[0]);
+});
 
 const verwijderActief = () => {
-    alleLinks.forEach ( (link) => {
-        if( link.classList = 'actief' ) {
+    alleLinks.forEach( (link) => {
+        if( link.classList = 'actief') {
             link.classList.remove('actief');
         }
     });
@@ -37,7 +38,6 @@ alleLinks.forEach( (link) => {
         window.location = e.target.href;
     })
 });
-
 
 const zoekBijpassendeLink = (id) => {
     let link = document.querySelector('nav a[href="' + id + '"]');
